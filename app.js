ea61066b819ad4f910346155d15ebefaef06a56c
ms_vague <haha.face@gmail.com>
2017-02-15 11:55:07 -0600

    created new repo for API Hack project

---
+++ app.js
@@ -0,0 +1,37 @@
+$(function(){
+
+    var googleFontUrl = 'https://www.googleapis.com/webfonts/v1/webfonts?key=AIzaSyBxzFarVQQ7DZrN8SDz4wMuikwd4Abx51w';
+
+    function getData(searchTerm, callback){
+        var query = {
+            s: searchTerm,  
+            r: 'json'
+        }
+        $.getJSON(googleFontUrl, query, callback);
+
+    }
+
+    function renderSearchData(data){
+        //alert('Test Submit Button');
+        var results = '<li>' + 'No results. Try Again.' + '</li>';
+
+        if (data.Search) {
+            console.log(data);
+            results = data.Search.map(function(item){
+                return '<li>' + item + '</li>';
+            });
+        }
+        $('.fonts').html(results);
+    }
+
+    function submit(){
+        $('form').submit(function(event){
+            event.preventDefault();
+            var searchTerm = $('.query_search').val();
+            getData(searchTerm, renderSearchData);
+        });
+    }
+
+    submit();
+
+});
