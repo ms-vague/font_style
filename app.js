416a0a160c9eab9687b5684afe676fcbfd8daecd
ms_vague <haha.face@gmail.com>
2017-03-24 16:29:59 -0500

    fixed webfontconfig to work with webfont.load

--- app.js
+++ app.js
@@ -1,120 +1,46 @@
 $(function(){
-    //console.log(WebFont);//
-
+    
     var googleFontUrl = 'https://www.googleapis.com/webfonts/v1/webfonts?key=AIzaSyBxzFarVQQ7DZrN8SDz4wMuikwd4Abx51w';
 
     $.getJSON(googleFontUrl, function(data) {
         var getArray = data.items;
-        // console.log(getArray);
+        var fontsArray = [];
+        var filesArray = [];
         for (var i = 0; i < getArray.length; i++) {
-            var getFamilies = getArray[i]['family'];
-            var getFiles = getArray[i]['files'];
-            // console.log(getFamilies);
-            // console.log(getFiles);
-            WebFont.load({
-                google: {
-                    families: ['Diplomata SC', 'Lato'] // not working //
-                }
-            });        
-        //var pushFamilies = WebFont.google.families.push(getFamilies);//
-        //var randomFamily = Math.floor(Math.random()*getFamilies.length);//
-        //console.log(pushFamilies);//
+            fontsArray.push(getArray[i]['family']);
+            filesArray.push(getArray[i]['files']['regular']);
+        } 
+        //console.log(fontsArray);    
+        //console.log(typeof joined);
+
+    var WebFontConfig = {
+        google: {
+            families: fontsArray
+        },
+        fontloading: function(familyName, fvd) {
+            console.log(familyName);
         }
-    });
-    $('.switch_button_one').click(function(){
-        //alert('Fuck me! It works!');//
-        $('.title_one').css('font-family', 'Diplomata SC'); // getFamlies not working //
-        $('.paragraph_one').css('font-family', 'Lato'); // getFamilies not working //
-    });
+    }    
+    WebFont.load(WebFontConfig);  
 
-});
-
-    
-
-
-    /* function renderSearchData(data){
-        //alert('Test Submit Button');
-
-        function getData(){
-        var query = {
-            sort: 'popularity'
-        }
-        $.getJSON(googleFontUrl, query);
-    }
-        
-        // nest this function with line 61 function //        
-        // developer.telerik //
-        // google: { [] } family will be a variable //
-        /* var WebFontConfig = {
-            google: { 
-                var FontFamilies = [];  
-            },
-            timeout: 2000 // font request abadoned if longer than 2 seconds //
-        }; 
-        //console.log(data);//
-        var getItems = data.items;
-        //console.log(getItems);//
-        for (var i = 0; i < getItems.length; i++) {
-            //console.log(getItems[i]['category']);//
-            var eachItemCat = getItems[i]['category'];
-            var eachItemFam = getItems[i]['family'];
-            var eachItemFile = getItems[i]['files'].regular;
-            console.log(eachItemCat);
-            console.log(eachItemFam);
-            console.log(eachItemFile);            
-        }
-        $('.font_pair').html(results);
-    }
-
-    function submit(){
-        $('switch_button').submit(function(event){
-            event.preventDefault();
-            getData(renderSearchData);
+    function switchButtons(){
+        $('.switch_button_one').click(function(){
+            $('.title_one').css('font-family', 'something'); 
+            $('.paragraph_one').css('font-family', 'something');
         });
-    }
-
-    submit();
-});
-
-
-
-/* // CSS jQUERY //
-$(document).ready(function(){
-     $('.foo, .bar').css({
-        fontsize: '200px',
-        color: 'red'
+        $('.switch_button_two').click(function(){
+            $('.title_two').css('font-family', 'something');
+            $('.paragraph_two').css('font-family', 'something');
+        });
+        $('.switch_button_three').click(function(){
+            $('.title_three').css('font-family', 'something'); 
+            $('.paragraph_three').css('font-family', 'something');
+        });
+        $('.switch_button_four').click(function(){
+            $('.title_four').css('font-family', 'something'); 
+            $('.paragraph_four').css('font-family', 'something');
+        });
+        }
+        switchButtons();
     });
 });
-
-// developer.telerik //
-
-// asynchronous approach //
- webFont.load({
-    google: {
-        families: []
-    }
-});
-
-var WebFontConfig = {
-    google: { 
-        families: eachItemFam
-    },
-    timeout: 2000 // font request abadoned if longer than 2 seconds //
-}; 
-
-(function(){
-    var wf = document.createElement('script');
-    wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
-        '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
-    wf.async = 'true';
-    document.head.appendChild(wf);
-})();
-
-// vanilla JS //
-document.getElementById('switch-button').addEventListener('click', function () {
-    if (document.documentElement.className.indexOf('wf-active') > -1) {
-        document.documentElement.className = '';
-    } else {
-        document.documentElement.className = 'wf-active';
-    }
-}); */
