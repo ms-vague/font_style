3414354de69216206e429149028a78fb8a214b16
ms_vague <haha.face@gmail.com>
2017-03-26 20:38:33 -0500

    randomFont not working, breaking font string into seperate strings

--- app.js
+++ app.js
@@ -10,37 +10,33 @@ $(function(){
             fontsArray.push(getArray[i]['family']);
             filesArray.push(getArray[i]['files']['regular']);
         } 
-        //console.log(fontsArray);    
-        //console.log(typeof joined);
+        //console.log(typeof fontsArray);    
+        //console.log(typeof joined); 
+
+    var randomFont = fontsArray[Math.floor(Math.random() * fontsArray.length)];
+    console.log(randomFont);     
 
     var WebFontConfig = {
         google: {
-            families: fontsArray
+            families: randomFont
         },
         fontloading: function(familyName, fvd) {
-            console.log(familyName, fvd);
+            //console.log(familyName);
+            switchButtons(familyName);
         }
-    }    
+    }
     WebFont.load(WebFontConfig);  
-
-    function switchButtons(){
-        $('.switch_button_one').click(function(){
-            $('.title_one').css('font-family', 'something'); 
-            $('.paragraph_one').css('font-family', 'something');
-        });
-        $('.switch_button_two').click(function(){
-            $('.title_two').css('font-family', 'something');
-            $('.paragraph_two').css('font-family', 'something');
-        });
-        $('.switch_button_three').click(function(){
-            $('.title_three').css('font-family', 'something'); 
-            $('.paragraph_three').css('font-family', 'something');
-        });
-        $('.switch_button_four').click(function(){
-            $('.title_four').css('font-family', 'something'); 
-            $('.paragraph_four').css('font-family', 'something');
-        });
-        }
-        switchButtons();
     });
+
+    var buttons = $('.switch_button_one, .switch_button_two, .switch_button_three, .switch_button_four');
+    var titles = $('.title_one, .title_two, .title_three, .title_four');
+    var paragraphs = $('.paragraph_one, .paragraph_two, .paragraph_three, .paragraph_four');
+
+    function switchButtons(font){
+        $(buttons).click(function(){ 
+            //alert('Works!'); 
+            $(titles).css('font-family', font);
+            $(paragraphs).css('font-family', font);
+        });
+    }
 });
