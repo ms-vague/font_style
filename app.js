8b799e83ce675be5764485611478014edc76e7fb
ms_vague <haha.face@gmail.com>
2017-03-29 11:12:56 -0500

    moved button click to change font before line  webfontload

--- app.js
+++ app.js
@@ -10,37 +10,48 @@ $(function(){
             fontsArray.push(getArray[i]['family']);
             filesArray.push(getArray[i]['files']['regular']);
         } 
-        //console.log(fontsArray);    
-        //console.log(typeof joined);
+        //console.log(typeof fontsArray);    
+        //console.log(typeof joined); 
+
+    
+    //console.log(oneFont);     
 
     var WebFontConfig = {
         google: {
-            families: fontsArray
+            families: getRandomFont(fontsArray)   // one random font array 
         },
-        fontloading: function(familyName, fvd) {
-            console.log(familyName, fvd);
+        fontloading: function(familyName, fvd) {   // takes array and breaks it up into separate font strings
+            //console.log(familyName);
+            switchStyle(familyName);
         }
-    }    
-    WebFont.load(WebFontConfig);  
-
-    function switchButtons(){
-        $('.switch_button_one').click(function(){
-            $('.title_one').css('font-family', 'something'); 
-            $('.paragraph_one').css('font-family', 'something');
-        });
-        $('.switch_button_two').click(function(){
-            $('.title_two').css('font-family', 'something');
-            $('.paragraph_two').css('font-family', 'something');
-        });
-        $('.switch_button_three').click(function(){
-            $('.title_three').css('font-family', 'something'); 
-            $('.paragraph_three').css('font-family', 'something');
-        });
-        $('.switch_button_four').click(function(){
-            $('.title_four').css('font-family', 'something'); 
-            $('.paragraph_four').css('font-family', 'something');
-        });
-        }
-        switchButtons();
+    }
+    $('.switch_button_one, .switch_button_two, .switch_button_three, .switch_button_four').click(function() {
+        WebFont.load(WebFontConfig);
+        });  
     });
+
+    /*var buttons = $('.switch_button_one, .switch_button_two, .switch_button_three, .switch_button_four');*/
+    /*var titles = $('.title_one, .title_two, .title_three, .title_four');*/
+    /*var paragraphs = $('.paragraph_one, .paragraph_two, .paragraph_three, .paragraph_four');*/
+
+    function getRandomFont(fontArray) {
+        //console.log(fontArray);
+        var min = 1;
+        var max = fontArray.length;
+        var randomFont = [];
+        randomFont.push(fontArray[Math.floor(Math.random() * (max - min + 1)) + min]);
+        //console.log(randomFont);
+        return randomFont;
+    }
+
+    function switchStyle(font){
+        $('.title_one').css('font-family', font);
+        $('.paragraph_one').css('font-family', font);
+        $('.title_two').css('font-family', font);
+        $('.paragraph_two').css('font-family', font);
+        $('.title_three').css('font-family', font);
+        $('.paragraph_three').css('font-family', font);
+        $('.title_four').css('font-family', font);
+        $('.paragraph_four').css('font-family', font);
+    }
 });
