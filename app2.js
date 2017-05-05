c7e06f17539f2080d3ca53d35ee64f968e5438b4
ms_vague <haha.face@gmail.com>
2017-05-05 12:47:05 -0500

    working on jQuery custom events for font click change

--- app2.js
+++ app2.js
@@ -1,58 +1,75 @@
-    // variable for dataAttr //
+var pageDataAttr = 'data_id';  
 
-    var pageDataAttr = 'data_id';  
+var pageElement = $('.pages');
+   //console.log($('.pages'));
+   //console.log(pageElement);
 
-    var pageElement = $('.pages');
-    //console.log($('.pages'));
-    //console.log(pageElement);
+   // variable to hold pageTemplate //
 
-    // variable to hold pageTemplate //
+var pagesTemplate = (
+      "<div class='page_container'>" +
+        "<div class='page_heading'>" +
+          "<h1 class='on'>" +
+          "</h1>" +
+        "</div>" +
+        "<div class='page_paragraph'>" +
+          "<p class='on'>" +
+          "</p>" +
+        "</div>" +
+      "</div>"
+   );
 
-    var pagesTemplate = (
-            "<div class='page'>" +
-                "<h1>" +
-                "</h1>" +
-                "<p>" + 
-                "</p>" +
-             "</div>"
-      );
+   // global state object //
 
-    // global state object //
+var state = {
+   pages: []    
+};
 
-    var state = {
-        pages: []    
-    };
+function addPage(heading, paragraph, id) {
+   state.pages.push({
+     heading: heading,
+     paragraph: paragraph,
+     id: id
+   });
+}
 
-    function addPage(heading, paragraph, id) {
-        state.pages.push({
-          heading: heading,
-          paragraph: paragraph,
-          id: id
-        });
-    }
+function getPage(state, pageIndex) {
+   return state.pages[pageIndex];
+}
 
-    function getPage(state, pageIndex) {
-      return state.pages[pageIndex];
-    }
+   // DOM manipulation //
 
-    // DOM manipulation //
+function renderPage(page, pageIndex, pageTemplate, pageDataAttr, font) {   /* function renders the page element on the DOM */
+   var element = $(pageTemplate);
+   element.find('h1').text(page.heading).css('font-family', font);
+   element.find('p').text(page.paragraph).css('font-family', font);
+   element.attr(pageDataAttr, pageIndex);
+   return element;
+}
 
-    function renderPage(page, pageIndex, pageTemplate, pageDataAttr) {   /* function renders the page element on the DOM */
-      var element = $(pageTemplate);
-      element.find('h1').text(page.heading);
-      element.find('p').text(page.paragraph);
-      element.attr(pageDataAttr, pageIndex);
-      return element;
-    }
-    
-    function renderPages(state, pageElement, pageDataAttr) {
-      var pagesHTML = state.pages.map(
-        function(page, pageIndex) {
-          return renderPage(page, pageIndex, pagesTemplate, pageDataAttr); 
-      });
-      pageElement.html(pagesHTML);
-    }
+function renderPages(state, pageElement, pageDataAttr, font) {
+   var pagesHTML = state.pages.map(
+     function(page, pageIndex) {
+     return renderPage(page, pageIndex, pagesTemplate, pageDataAttr, font); 
+   });
+   pageElement.html(pagesHTML);
+}
 
-    addPage('this is heading', 'paragraph content', 0);
-    addPage('more content for heading', 'this is additional content for paragraph', 1);
-    renderPages(state, pageElement, pageDataAttr);
+addPage('Heading', 'Paragraph', 0);
+
+   // event handling //
+
+$('h1').on('font:change', function(event) {
+  var headingStyle = $(this);
+  if (headingStyle.is('.on')) {
+    headingStyle.removeClass('on').addClass('off');
+  } else {
+    headingStyle.removeClass('off').addClass('on'); 
+  }
+});
+
+$('.button_h').click(function() {
+  var heading = $(this).closest('.pages');
+  heading.find('h1').trigger('font:change');
+});
+
