4d66918cee4f9b4ab6c99013aac38ebc0d45b7fe
ms_vague <haha.face@gmail.com>
2017-05-05 23:12:55 -0500

    jQuery css toggling font change but not from google api

--- app2.js
+++ app2.js
@@ -37,60 +37,57 @@ function getPage(state, pageIndex) {
 
    // DOM manipulation //
 
-function renderPage(page, pageIndex, pageTemplate, pageDataAttr, font) {   /* function renders the page element on the DOM */
+function renderPage(page, pageIndex, pageTemplate, pageDataAttr) {   /* function renders the page element on the DOM */
    var element = $(pageTemplate);
-   element.find('h1').text(page.heading).css('font-family', font);
-   element.find('p').text(page.paragraph).css('font-family', font);
+   element.find('h1').text(page.heading);
+   element.find('p').text(page.paragraph);
    element.attr(pageDataAttr, pageIndex);
    return element;
 }
 
-function renderPages(state, pageElement, pageDataAttr, font) {
+function renderPages(state, pageElement, pageDataAttr) {
    var pagesHTML = state.pages.map(
      function(page, pageIndex) {
-     return renderPage(page, pageIndex, pagesTemplate, pageDataAttr, font); 
+     return renderPage(page, pageIndex, pagesTemplate, pageDataAttr); 
    });
    pageElement.html(pagesHTML);
 }
 
 addPage('Heading', 'Paragraph', 0);
+renderPages(state, pageElement, pageDataAttr);
 
    // custom event handling //
 
    // heading class toggle //
 
-$('.pages').on('heading:toggle', '.heading', function(event) {
-  var heading = $(this);
-  if (heading.is('.on')) {
-    heading.removeClass('on').addClass('off');
-  } else {
-    heading.removeClass('off').addClass('on');
-  }
-});
-
-$('.pages').on('click', '.button_h', function(event) {
-  event.preventDefault();
-  var container = $(this).closest('.page_container');
-  container.find('.heading').trigger('heading:toggle', {
-    $element:container.find('.heading')
+$(function() {
+  $('.pages').on('heading:toggle', '.heading', function(event) {
+    var heading = $(this);
+    if (heading.is('.on')) {
+      heading.removeClass('on').addClass('off').css('font-family', 'new courier');
+    } else {
+      heading.removeClass('off').addClass('on').css('font-family', 'garamond');
+    }
   });
-});
+
+  $('.pages').on('click', '.button_h', function() {
+    var container = $(this).closest('.page_container');
+    container.find('.heading').trigger('heading:toggle');
+  });
 
   // paragraph class toggle //
 
-  $('.pages').on('heading:toggle', '.paragraph', function(event) {
-  var paragraph = $(this);
-  if (paragraph.is('.on')) {
-    paragraph.removeClass('on').addClass('off');
-  } else {
-    paragraph.removeClass('off').addClass('on');
-  }
-});
+    $('.pages').on('heading:toggle', '.paragraph', function(event) {
+    var paragraph = $(this);
+    if (paragraph.is('.on')) {
+      paragraph.removeClass('on').addClass('off').css('font-family', 'arial');
+    } else {
+      paragraph.removeClass('off').addClass('on').css('font-family', 'verdana');
+    }
+  });
 
-$('.pages').on('click', '.button_p', function(event) {
-  event.preventDefault();
-  var container = $(this).closest('.page_container');
-  container.find('.paragraph').trigger('heading:toggle', {
-    $element:container.find('.paragraph')
+  $('.pages').on('click', '.button_p', function() {
+    var container = $(this).closest('.page_container');
+    container.find('.paragraph').trigger('heading:toggle');
   });
 });
