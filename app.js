abe9d57613b3c543a5735efa2d23090a3fe96468
ms_vague <haha.face@gmail.com>
2017-06-02 19:07:27 -0500

    h1 and p fonts change on every click

--- app.js
+++ app.js
@@ -1,201 +1,195 @@
 var googleFontUrl = 'https://www.googleapis.com/webfonts/v1/webfonts?key=AIzaSyBxzFarVQQ7DZrN8SDz4wMuikwd4Abx51w';
 
+   // global state object //
+
 var state = {
-   pages: []  
+   pages: [], 
+   fonts: [],
+   loadedFonts: []
 };
 
 $.getJSON(googleFontUrl, function(data) {
    var getArray = data.items;
-   var fontsArray = [];
    var filesArray = [];
    for (var i = 0; i < getArray.length; i++) {
-       fontsArray.push(getArray[i]['family']);
+       state.fonts.push(getArray[i]['family']);
        filesArray.push(getArray[i]['files']['regular']);
    } 
 
-function makeWebFontConfig() { 
-   return {
-       google: {
-           families: getRandomFont(fontsArray)
-       },
-       fontloading: function(familyName, fvd) {
-       //console.log(familyName);  
-       }
-   }
-}
-WebFont.load(makeWebFontConfig());
+  function makeWebFontConfig() {
+  //console.log(state.fonts); 
+    return {
+      google: {
+          families: [getRandomFont(state.fonts)]
+        },
+        fontloading: function(familyName, fvd) {     
+          console.log(familyName);
+          addLoadedFont(familyName);
+      }
+    }
+  } 
+  WebFont.load(makeWebFontConfig());
 
-function getRandomFont(fontArray) {
-   var min = 1;
-   var max = fontArray.length;
-   var randomFont = [];
-   randomFont.push(fontArray[Math.floor(Math.random() * (max - min + 1)) + min]);
-   //console.log(randomFont);
-   return randomFont;
-}
+  function getRandomFont(fontArray) {
+    //console.log(fontArray);
+     var min = 1;
+     var max = fontArray.length;
+     var randomFont = [];
+     randomFont.push(fontArray[Math.floor(Math.random() * (max - min + 1)) + min]);
+     var stringFont = randomFont.join('');
+     //console.log(stringFont);
+     return stringFont;
+  }
+  
+  var pageDataAttr = 'data_id';  
 
-var pageDataAttr = 'data_id';  
+  var pageElement = $('.pages');
 
-var pageElement = $('.pages');
-   //console.log($('.pages'));
-   //console.log(pageElement);
+     // variable to hold pageTemplate //
 
-   // variable to hold pageTemplate //
+  var pagesTemplate = (
+          "<div class='page_container'>" +
+            "<button class='button_h'>Heading font</button>" +
+              "<h1 class='heading on'>" +
+              "</h1>" +
+              "<p class='paragraph on'>" +
+              "</p>" +
+              "<button class='button_p'>Paragraph font</button>" +
+              "<span><textarea class='hidden heading-changer' type='text' placeholder='Add heading text'></textarea></span>" +
+              "<span><textarea class='hidden paragraph-changer' type='text' placeholder='Add paragraph text'></textarea></span>" +
+            "<button class='delete'>" +
+                "<span>Delete page</span>" +
+            "</button>" +
+          "</div>" 
+     );
 
-var pagesTemplate = (
-        "<div class='page_container'>" +
-          "<button class='button_h'>Heading font</button>" +
-            "<h1 class='heading on'>" +
-            "</h1>" +
-            "<span><textarea class='hidden heading-changer' type='text' placeholder='Change heading'></textarea></span>" +
-          "<button class='button_p'>Paragraph font</button>" +
-            "<p class='paragraph on'>" +
-            "</p>" +
-            "<span><textarea class='hidden paragraph-changer' type='text' placeholder='Change paragraph'></textarea></span>" +
-          "<button class='delete'>" +
-              "<span>Delete page</span>" +
-          "</button>" +
-        "</div>" 
-   );
+  function addPage(spec) {
+     state.pages.push(spec);
+  }
 
-   // global state object //
+  function getPage(state, pageIndex) {
+     return state.pages[pageIndex];
+  }
 
-function addPage(spec) {
-   state.pages.push(spec);
-}
+  function updatePage(state, pageIndex, newSpecs) {
+     Object.keys(newSpecs).forEach(function(spec) {
+      state.pages[pageIndex][spec] = newSpecs[spec];
+     });
+  };
 
-function getPage(state, pageIndex) {
-   return state.pages[pageIndex];
-}
+  function deletePage(state, pageIndex) {
+    state.pages.splice(pageIndex, 1);
+  };
 
-function updatePage(state, pageIndex, newSpecs) {
-   Object.keys(newSpecs).forEach(function(spec) {
-    state.pages[pageIndex][spec] = newSpecs[spec];
-   });
-};
+     // DOM manipulation //
 
-function deletePage(state, pageIndex) {
-  state.pages.splice(pageIndex, 1);
-};
+  function renderPage(page, pageIndex, pageTemplate, pageDataAttr) {   /* function renders the page element on the DOM */
+     var element = $(pageTemplate);
+     element.find('h1')
+      .text(page.heading);
+      renderHeadingFont(page, element.find('h1'));
+     element.find('p')
+      .text(page.paragraph)
+      renderParagraphFont(page, element.find('p'));
+     element.attr(pageDataAttr, pageIndex);
+     return element;
+  }
 
-   // DOM manipulation //
+  function addLoadedFont(familyName) {
+      state.loadedFonts.push(familyName);
+  }
 
-function renderPage(page, pageIndex, pageTemplate, pageDataAttr) {   /* function renders the page element on the DOM */
-   var element = $(pageTemplate);
-   element.find('h1')
-    .text(page.heading)
-    .css('font-family', page.headingFont);
-   element.find('p')
-    .text(page.paragraph)
-    .css('font-family', page.paragraphFont);
-   element.attr(pageDataAttr, pageIndex);
-   return element;
-}
+  function renderHeadingFont(page, element) {
+    return element.css('font-family', page.headingFont);
+  }
 
-function renderPages(state, pageElement, pageDataAttr) {
-   var pagesHTML = state.pages.map(
-     function(page, pageIndex) {
-     return renderPage(page, pageIndex, pagesTemplate, pageDataAttr); 
-   });
-   pageElement.html(pagesHTML);
-}
+  function renderParagraphFont(page, element) {
+    return element.css('font-family', page.paragraphFont);
+  }
 
-/* show Ben */
+  function renderPages(state, pageElement, pageDataAttr) {
+     var pagesHTML = state.pages.map(
+       function(page, pageIndex) {
+       return renderPage(page, pageIndex, pagesTemplate, pageDataAttr); 
+     });
+     pageElement.html(pagesHTML);
+  }
 
-function handleAddPage(spec) {
-  addPage(spec);
+    // event listeners //
+
+  function handleAddPage(spec) {
+    addPage(spec);
+    renderPages(state, pageElement, pageDataAttr);
+  }
+
+  function handleHeadingText(pageDataAttr, pageElement, state) {
+    $('.pages').on('keyup', '.heading-changer', function(event) {
+      event.preventDefault();
+      var pageIndex = parseInt($(this).closest('.page_container').attr(pageDataAttr));
+      //console.log(($(this).val()));
+      if (event.which === 13) { 
+        updatePage(state, pageIndex, { heading: $(this).val() });
+        renderPages(state, pageElement, pageDataAttr);
+      }
+    });
+  }
+
+  function handleParagraphText(pageDataAttr, pageElement, state) {
+    $('.pages').on('keyup', '.paragraph-changer', function(event) {
+      event.preventDefault();
+      var pageIndex = parseInt($(this).closest('.page_container').attr(pageDataAttr));
+      //console.log(($(this).val()));
+      if (event.which === 13) { 
+        updatePage(state, pageIndex, { paragraph: $(this).val() });
+        renderPages(state, pageElement, pageDataAttr);
+      }
+    });
+  }
+
+  function handleHeadingFont(pageDataAttr, pageElement, state, font) {
+    $('.pages').on('click', '.button_h', function(event) {
+      event.preventDefault();
+      var pageIndex = parseInt($(this).closest('.page_container').attr(pageDataAttr));
+      WebFont.load(makeWebFontConfig());
+      console.log(state.loadedFonts);
+      updatePage(state, pageIndex, { headingFont: state.loadedFonts.splice(0, 818) }); 
+      renderPages(state, pageElement, pageDataAttr);
+    });
+  }
+  
+
+  function handleParagraphFont(pageDataAttr, pageElement, state, font) {
+    $('.pages').on('click', '.button_p', function(event) {
+      event.preventDefault();
+      var pageIndex = parseInt($(this).closest('.page_container').attr(pageDataAttr));
+      WebFont.load(makeWebFontConfig());
+      console.log(state.loadedFonts);
+      updatePage(state, pageIndex, { paragraphFont: state.loadedFonts.splice(0, 818) });
+      renderPages(state, pageElement, pageDataAttr);
+    });
+  }
+
+  function handlePageDeletes(pageDataAttr, pageElement, state) {
+    $('.pages').on('click', '.delete', function(event) {
+      var pageIndex = parseInt($(this).closest('.page_container').attr(pageDataAttr));
+      deletePage(state, pageIndex);
+      renderPages(state, pageElement, pageDataAttr);
+    });
+  }
+
   renderPages(state, pageElement, pageDataAttr);
-}
+  //handleAddPage({}); 
+  handleHeadingText(pageDataAttr, pageElement, state);
+  handleParagraphText(pageDataAttr, pageElement, state);
+  handleHeadingFont(pageDataAttr, pageElement, state);
+  handleParagraphFont(pageDataAttr, pageElement, state);
+  handlePageDeletes(pageDataAttr, pageElement, state);   
 
-  /* show Ben */
-
-function handleFontChange(pageDataAttr, pageElement, state, font) {
-  $('.pages').on('click', '.button_h', function(event) {
-    event.preventDefault();
-    var pageIndex = parseInt($(this).closest('.page_container').attr(pageDataAttr));
-    updatePage(state, pageIndex, { headingFont: font }, {paragraphFont: font});
-    renderPages(state, pageElement, pageDataAttr);
+  $(function() {
+    $('.add').fadeIn('slow')
+      .on('click', function() {
+      handleAddPage({});
+    });
   });
-}
+});  
 
-function handleHeadingUpdates(pageDataAttr, pageElement, state) {
-  $('.pages').on('keyup', '.heading-changer', function(event) {
-    event.preventDefault();
-    var pageIndex = parseInt($(this).closest('.page_container').attr(pageDataAttr));
-    //console.log(($(this).val()));
-    if (event.which === 13) { 
-      updatePage(state, pageIndex, { heading: $(this).val() });
-      renderPages(state, pageElement, pageDataAttr);
-    }
-  });
-}
-
-function handleParagraphUpdates(pageDataAttr, pageElement, state) {
-  $('.pages').on('keyup', '.paragraph-changer', function(event) {
-    event.preventDefault();
-    var pageIndex = parseInt($(this).closest('.page_container').attr(pageDataAttr));
-    //console.log(($(this).val()));
-    if (event.which === 13) { 
-      updatePage(state, pageIndex, { paragraph: $(this).val() });
-      renderPages(state, pageElement, pageDataAttr);
-    }
-  });
-}
-
-function handlePageDeletes(pageDataAttr, pageElement, state) {
-  $('.pages').on('click', '.delete', function(event) {
-    var pageIndex = parseInt($(this).closest('.page_container').attr(pageDataAttr));
-    deletePage(state, pageIndex);
-    renderPages(state, pageElement, pageDataAttr);
-  });
-}
-
-renderPages(state, pageElement, pageDataAttr);
-handleAddPage({});
-handleFontChange(pageDataAttr, pageElement, state);
-handleHeadingUpdates(pageDataAttr, pageElement, state);
-handleParagraphUpdates(pageDataAttr, pageElement, state);
-handlePageDeletes(pageDataAttr, pageElement, state);   
-
-$(function() {
-  $('.add').on('click', function() {
-    handleAddPage({});
-  });
-});
-
-   // custom event handling //
-
-   // heading class toggle //
-/*$(function() {
-  $('.pages').on('heading:toggle', '.heading', function(event) {
-    var heading = $(this);
-    if (heading.is('.on')) {
-      heading.removeClass('on').addClass('off');
-    } else {
-      heading.removeClass('off').addClass('on');
-    }
-  });
-
-  $('.pages').on('click', '.button_h', function() {
-    var container = $(this).closest('.page_container');
-    container.find('.heading').trigger('heading:toggle');
-  });
-
-  // paragraph class toggle //
-
-  $('.pages').on('heading:toggle', '.paragraph', function(event) {
-    var paragraph = $(this);
-    if (paragraph.is('.on')) {
-      paragraph.removeClass('on').addClass('off');
-    } else {
-      paragraph.removeClass('off').addClass('on');
-    }
-  });
-
-  $('.pages').on('click', '.button_p', function() {
-    var container = $(this).closest('.page_container');
-    container.find('.paragraph').trigger('heading:toggle');
-  });  
-});*/
-//console.log(state.pages);
-});
